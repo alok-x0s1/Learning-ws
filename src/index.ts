1527e29c612da1a38fed2fa42183210cffa18cde
Alok Yadav <yalok6321@gmail.com>
2024-10-08 16:01:30 +0530

    Http and ws server config

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import http from "http";
+import { WebSocketServer, WebSocket } from "ws";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
+import Room from "./models/room";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+// Middlewares
+app.use(express.json());
+app.use(
+	cors({
+		origin: ["http://localhost:5173"],
+		credentials: true,
+	})
+);
+
+// Routes
+app.post("/api/rooms", async (req: Request, res: Response) => {
+	const { customUrl } = req.body;
+	const room = new Room({
+		id: uuidv4(),
+		customUrl: customUrl || uuidv4(),
+	});
+	await room.save();
+	res.json({ roomId: room.id, customUrl: room.customUrl });
+});
+
+// WebSocket handling
+const clients = new Map();
+
+wss.on("connection", (ws: WebSocket) => {
+	const clientId = uuidv4();
+	const metadata = { id: clientId };
+
+	clients.set(ws, metadata);
+
+	ws.on("message", (messageAsString: string) => {
+		const message = JSON.parse(messageAsString);
+		const metadata = clients.get(ws);
+
+		message.sender = metadata.id;
+		const outbound = JSON.stringify(message);
+
+		[...clients.keys()].forEach((client) => {
+			client.send(outbound);
+		});
+	});
+
+	ws.on("close", () => {
+		clients.delete(ws);
+	});
+});
+
+// Server started
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+	console.log(`Server is starting at port : http://localhost:${port}`);
+});
