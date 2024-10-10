1d10c3f8b8ab3f91683f164d6db39df658bacc67
Alok Yadav <yalok6321@gmail.com>
2024-10-10 11:18:03 +0530

    App config for react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import CreateRoom from "./components/CreateRoom";
 import ChatRoom from "./components/ChatRoom";
+import HomePage from "./components/Home";
 
 const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<CreateRoom />} />
+			<Route path="/" element={<HomePage />} />
+			<Route path="/room" element={<CreateRoom />} />
 			<Route path="/room/:roomId" element={<ChatRoom />} />
 		</Routes>
 	);
