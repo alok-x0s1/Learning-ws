b75e6d30698741786056f8d70ed892ae9b2653e2
Alok Yadav <yalok6321@gmail.com>
2024-10-10 11:17:25 +0530

    Add favicon and config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: "#C4BCB2",
+				foreground: "#1C1C19",
+			},
+		},
 	},
 	plugins: [],
 };
